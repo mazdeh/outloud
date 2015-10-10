a331d02b81b2d4b507fca575634166a0b951900d
Frederik Lohner <freddylohner@gmail.com>
2015-10-10 12:07:31 -0600

    Changed folder name for clarification purposes.

---
+++ iOSOutloud/Outloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Outloud
+//
+//  Created by Frederik Lohner on 10/Oct/15.
+//  Copyright © 2015 MVP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
